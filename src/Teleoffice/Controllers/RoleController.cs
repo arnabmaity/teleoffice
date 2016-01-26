3a464c669b251930683aec499fd98b0006d0b453
Ruchi Dhage <ruchiii.dhage@outlook.com>
2016-01-26 22:50:10 +0530

    only roles added

---
+++ src/Teleoffice/Controllers/RoleController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Teleoffice.Models;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Teleoffice.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public RoleController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+           var roles = context.Roles.OrderBy(r => r.Name).ToArray();
+           return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IdentityRole role)
+        {
+            try
+            {
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = role.Name
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
