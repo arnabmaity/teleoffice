a70d43762fc29a33fedb7641d12ddfa69f2c3265
Ruchi Dhage <ruchiii.dhage@outlook.com>
2016-02-19 18:38:05 +0530

    ...

---
+++ src/Teleoffice/ViewModels/Professional/DeclineMessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Teleoffice.Models;
+
+namespace Teleoffice.ViewModels.Professional
+{
+    public class DeclineMessageViewModel
+    {
+        [Required]
+        public int ProfessionalId { get; set; }
+
+        [Required]
+        public String Message { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public IList<Notification> Notifications { get; set; }
+        public ApplicationUser Users { get; set; }
+
+    }
+}
